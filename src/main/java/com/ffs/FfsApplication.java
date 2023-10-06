948ac9f63ec43ed9e4476de18a037cb93567613e
hoshinosena <hihoshinosena@gmail.com>
2023-10-06 22:16:55 +0900

    bug fixes for usercache

--- src/main/java/com/ffs/FfsApplication.java
+++ src/main/java/com/ffs/FfsApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class FfsApplication {
 	public static void main(String[] args) {
-		UserCache.enable();
 		SpringApplication.run(FfsApplication.class, args);
+		UserCache.enable();
 	}
 }
